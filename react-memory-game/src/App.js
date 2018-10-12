fe29c238f4494cb58f518916f4fe21e294a14edf
ajschilt <ajschilt86@gmail.com>
2018-10-12 18:26:59 -0500

    working with pictures

--- react-memory-game/src/App.js
+++ react-memory-game/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
-import Card from "./components/Card";
-import Wrapper from "./components/Wrapper";
-import Title from "./components/Title";
-import Nav from "./components/Nav";
-import heroes from "./heroes.json"
 import './App.css';
+import heroes from "./heroes.json"
+import Wrapper from "./components/Wrapper";
+import Nav from "./components/Nav";
+import Title from "./components/Title";
+import Card from "./components/Card";
+
+
 
 class App extends Component {
   state = {
-    message: "Click an image to begin",
+    message: "Click an image to begin!",
     topScore: 0,
     curScore: 0,
     heroes: heroes,
@@ -27,19 +29,52 @@ class App extends Component {
   }
 
   selectHero = name => {
-    
+    const findHero = this.state.unselectedHeroes.find(character => character.name === name);
+
+    if (findHero === undefined) {
+      this.setState({
+        message: "you guessed incorrectly",
+        topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+        curScore: 0,
+        heroes: heroes,
+        unselectedHeroes: heroes
+      });
+    } else {
+      const newHeroes = this.state.unselectedHeroes.filter(character => character.name !== name);
+
+      this.setState({
+        message: "you guessed correctly",
+        curScore: this.state.curScore + 1,
+        heroes: heroes,
+        unselectedHeroes: newHeroes
+      });
+    }
+
+    this.randomizeArray(heroes);
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          message={this.state.message}
+          curScore={this.state.curScore}
+          topScore={this.state.topScore}
+        />
+        <Title />
+        {
+          this.state.heroes.map(hero => (
+            <Card
+              name={hero.name}
+              image={hero.image}
+              selectHero={this.selectHero}
+              curScore={this.state.curScore}
+            />
+          ))
+        }
+      </Wrapper>
+    );
   }
-
-
-
-
-
-
-
-
-
-
-
 }
 
 export default App;
